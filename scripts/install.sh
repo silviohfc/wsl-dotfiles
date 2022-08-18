f2355c83868072403c42a28026bc0c7d680881b6
Silvio Ferreira <silviohfc@gmail.com>
2022-08-18 02:48:43 -0300

    --amend

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,9 +16,9 @@ export GIT_NAME
 ~/.dotfiles/scripts/gitconfig.sh
 
 echo "Creating Symlinks..."
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 
 echo "Installing asdf with Node LTS..."
 ~/.dotfiles/scripts/asdf-node.sh
