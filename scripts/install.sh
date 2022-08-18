2916e00bda8b61c10046490bb4a72ad6979b9007
Silvio Ferreira <silviohfc@gmail.com>
2022-08-18 01:43:50 -0300

    add oh my zsh and p10k

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+echo "Installing Oh My Zsh..."
+~/.dotfiles/scripts/ohmyzsh.sh
+
+echo "Installing Powerlevel10k Theme..."
+~/.dotfiles/scripts/p10k.sh
+
 echo "What is your GIT email?"
 read GIT_EMAIL
 echo "What is your GIT name?"
 read GIT_NAME
 
+echo "Configuring GIT..."
 export GIT_EMAIL
 export GIT_NAME
 ~/.dotfiles/scripts/gitconfig.sh
---
+++ scripts/p10k.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
