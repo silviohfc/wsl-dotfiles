e4ec31a0be068859dc4569c00cdfa66933a330a6
Silvio Ferreira <silviohfc@gmail.com>
2022-08-18 02:21:00 -0300

    remove zsh and fix asdf

--- scripts/asdf-node.sh
+++ scripts/asdf-node.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs lts
--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-apt update
-apt install zsh -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
